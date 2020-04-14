4f6b56084a5d8b841e3b94427df2c66abc81efec
cho881020 <kj_cho@idealidea.co.kr>
2020-04-14 21:02:44 +0900

    EditText 내용 변경 감지 이벤트 처리

--- app/src/main/java/kr/tjeit/signuppractice_event_20200414/MainActivity.kt
+++ app/src/main/java/kr/tjeit/signuppractice_event_20200414/MainActivity.kt
@@ -2,6 +2,11 @@ package kr.tjeit.signuppractice_event_20200414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +18,31 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        idEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = s.toString()
+                if (inputVal.length == 0) {
+                    idStatusTxt.text = "최소 여섯글자 이상으로 해주세요."
+                }
+                else if (inputVal.length < 6) {
+                    idStatusTxt.text = "길이가 너무 짧습니다."
+                }
+                else {
+                    idStatusTxt.text = "사용해도 좋은 아이디입니다."
+                }
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
 
     }
 
